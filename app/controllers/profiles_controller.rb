39b5a3d4af9a517ab9322c58575175e84aac6d08
BlaxFontaine <45664066+BlaxFontaine@users.noreply.github.com>
2019-02-28 14:38:21 +0100

    Update profiles_controller.rb

--- app/controllers/profiles_controller.rb
+++ app/controllers/profiles_controller.rb
@@ -1,6 +1,6 @@
 class ProfilesController < ApplicationController
-  # skip_before_action :authenticate_user!, only: [:show, :edit, :update]
   def show
     @user = User.find(params[:id])
     authorize @user
   end
+end
