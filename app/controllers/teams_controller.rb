947287737050b56e7e9defc8839d017e4327cee5
Charlotte Rebillon <charlotte_rebillon@orange.fr>
2019-02-28 15:08:28 +0100

    ranking

--- app/controllers/teams_controller.rb
+++ app/controllers/teams_controller.rb
@@ -1,6 +1,6 @@
 class TeamsController < ApplicationController
   def index
-      @teams = policy_scope(Team)
+    @teams = policy_scope(Team)
     if params[:index]
       # get start_date from the form
       booking_start_date = Date.parse params[:index][:start_date]
@@ -20,8 +20,6 @@ class TeamsController < ApplicationController
         team_games_dates.include?(booking_start_date)
       end
 
-      # teams_displayed
-
       user_ranking = current_user.team.ranking
       # array of hashes (with team_id: & ranking_difference:)
       ranking_differences = []
@@ -31,25 +29,20 @@ class TeamsController < ApplicationController
         rankings << user_ranking
         rankings.sort! { |a, b| b <=> a }
         difference = rankings[0] - rankings[1]
-
         team_result = {
-                      the_team: Team.find_by_id(team.id),
-                      ranking_difference: difference
+                        the_team: Team.find_by_id(team.id),
+                        ranking_difference: difference
                       }
-
         ranking_differences << team_result
-        # sort an array of hash by ranking_difference
       end
-
+      # sort an array of hash by ranking_difference
       ranked_teams = ranking_differences.sort_by! { |computed_team_result| computed_team_result[:ranking_difference] }
       final_ranked_teams = []
       ranked_teams.each do |team|
         final_ranked_teams << team[:the_team]
       end
       @teams = final_ranked_teams
-        # team_result[:the_team_id]
     end
-
   end
 
   def show
