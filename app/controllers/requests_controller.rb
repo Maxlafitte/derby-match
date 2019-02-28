c8a419bf5d7ca23d64e8bfcdb0075f4fb5d1672b
Charlotte Rebillon <charlotte_rebillon@orange.fr>
2019-02-28 20:57:20 +0100

    next events & pending requests

--- app/controllers/requests_controller.rb
+++ app/controllers/requests_controller.rb
@@ -1,9 +1,9 @@
 class RequestsController < ApplicationController
   def index
-    # @requests_sent = policy_scope(Request).where(user: current_user)
-    # @requests_received = Request.all.select do |request|
-    #   request.team.user == current_user
-    # end
+    @requests_sent = policy_scope(Request).where(user: current_user)
+    @requests_received = Request.all.select do |request|
+      request.team.user == current_user
+    end
   end
 
   def show
