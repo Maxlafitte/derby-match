31c1405feeeb0f2418edd79070bc47ca6ce6d365
Valerie Gehle <valerie.gehle@gmail.com>
2019-03-01 14:45:56 +0100

    navabar pimped

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -2,6 +2,8 @@ import "bootstrap";
 import "../plugins/flatpickr";
 import 'select2/dist/css/select2.css';
 import { initSelect2 } from '../plugins/init_select2';
+import { active_class } from '../plugins/active_class';
 
 initSelect2();
-console.log('io')
+console.log('io');
+
