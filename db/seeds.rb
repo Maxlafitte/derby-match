4660a5ab4b7be2244655f36af6391814bdee65cf
mathilde ferrey <mathildeferrey@yahoo.com>
2019-03-07 09:53:43 +0100

    seeds supp

--- db/seeds.rb
+++ db/seeds.rb
@@ -25,7 +25,11 @@ stockholm_logo_url = 'http://flattrackstats.com/sites/default/files/imagecache/p
 stockholm_b_logo_url = "http://flattrackstats.com/sites/default/files/imagecache/profile/logos/stockholm\%20bstrd.jpg"
 liverpool_logo_url = 'http://flattrackstats.com/sites/default/files/imagecache/profile/logos/17359300_1346809895383605_4405284166842330556_o.jpg'
 yellow_shovemarines_logo_url = 'http://flattrackstats.com/sites/default/files/imagecache/profile/logos/Liverpool-Roller-Birds-150_0.png'
-
+amsterdam_logo_url = 'http://flattrackstats.com/sites/default/files/imagecache/profile/logos/a%27dam_0.png'
+brest_logo_url = 'http://flattrackstats.com/sites/default/files/imagecache/profile/logos/14705901_1465282670161678_7022213945138907968_n.jpg'
+brussels_logo_url = 'http://flattrackstats.com/sites/default/files/imagecache/profile/logos/_LOGO_Pixies_2013.png'
+brussels_b_logo_url = 'http://flattrackstats.com/sites/default/files/imagecache/profile/logos/Atomium%20Kittens.jpg'
+turku_logo_url = 'http://flattrackstats.com/sites/default/files/imagecache/profile/logos/15895313_1186430301406834_1278590317176866953_n.jpg'
 
 puts 'Creating leagues'
 league_1 = League.create!(name: 'Bear City Roller Derby', city: "Berlin", country: "Germany", logo: bear_city_logo_url, latitude: 52.52437, longitude: 13.41053, website: 'bearcityrollerderby.com/')
@@ -36,6 +40,11 @@ league_5 = League.create!(name: 'Beastie Derby Girls', city: "Reims", country: "
 league_6 = League.create!(name: 'Cornwall Roller Derby', city: "Newquay", country: "UK", logo: cornwall_logo_url, latitude: 50.4156, longitude: -5.073190000000068, website: 'www.cornwallrollerderby.com/')
 league_7 = League.create!(name: 'Stockholm Roller Derby', city: "Stockholm", country: "Sweden", logo: stockholm_logo_url, latitude: 59.3251172, longitude: 18.0710935, website: 'rollerderby.se/')
 league_8 = League.create!(name: 'Liverpool Roller Birds', city: "Liverpool", country: "UK", logo: liverpool_logo_url, latitude: 53.4106, longitude: 2.9779399999999896, website: 'www.liverpoolrollerbirds.co.uk/')
+
+league_9 = League.create!(name: 'Amsterdam Roller Derby', city: "Amsterdam", country: "Netherlands", logo: amsterdam_logo_url, latitude: 48.8648405, longitude: 2.3798485, website: 'www.amsterdamrollerderby.nl')
+league_10 = League.create!(name: 'B.M.O. Roller Derby Girls', city: "Brest", country: "France", logo: brest_logo_url, latitude: 48.3905283, longitude: -4.4860088, website: 'https://www.facebook.com/BMO.RDG/')
+league_11 = League.create!(name: 'Brussels Derby Pixies', city: "Brussels", country: "Belgium", logo: brussels_logo_url, latitude: 50.8465573, longitude: 4.351697, website: 'www.facebook.com/BrusselsDerbyPixies')
+league_12 = League.create!(name: 'Dirty River Roller Derby', city: "Turku", country: "Finland", logo: turku_logo_url, latitude: 60.4518, longitude: 22.267100000000028, website: 'turkurollerderby.com')
 
 puts 'Leagues created'
 
@@ -65,6 +74,16 @@ user_18 = User.create!(email: 'sisters@liverpool.com', password: 'azerty', derby
 user_19 = User.create!(email: 'yellow_shovemarines@liverpool.com', password: 'azerty', derby_name: 'La terreur des Yellow Shovemarines')
 user_20 = User.create!(email: 'c_team@liverpool.com', password: 'azerty', derby_name: 'La terreur de la C Team de Liverpool')
 
+user_21 = User.create!(email: 'myrtle@amsterdam.com', password: 'azerty', derby_name: 'Amsterdam Infernal Myrtle ')
+user_22 = User.create!(email: 'niagra@amsterdam.com', password: 'azerty', derby_name: 'Amsterdam Niagra Falls')
+user_23 = User.create!(email: 'jam@brest.com', password: 'azerty', derby_name: 'Brest Strawberry Jam')
+user_24 = User.create!(email: 'bambi@brest.com', password: 'azerty', derby_name: 'Brest Bambi Hunter')
+user_25 = User.create!(email: 'max@brussels.com', password: 'azerty', derby_name: 'Brussels Max Penalty')
+user_26 = User.create!(email: 'indominous@brussels.com', password: 'azerty', derby_name: 'Brussels Indominous')
+user_27 = User.create!(email: 'mascara@turku.com', password: 'azerty', derby_name: 'Turku Bleeding Mascara')
+user_28 = User.create!(email: 'devil@turku.com', password: 'azerty', derby_name: 'Turku Derby Devil')
+
+
 puts "#{User.count} users created"
 
 puts 'Creating teams'
@@ -80,7 +99,6 @@ teams = Team.create!([
   { name: 'Les quedalles', ranking: 83, league: league_3, user: user_8, photo: prg_logo_url },
   { name: 'Les sans culottes', ranking: 413, league: league_3, user: user_9, photo: prg_logo_url },
   { name: 'Les nasty pécheresses', ranking: 259, league: league_4, user: user_10, photo: montreuil_logo_url },
-
   { name: 'All stars', ranking: 171, league: league_5, user: user_12, photo: beastie_logo_url },
   { name: 'Atlantic Breakers A', ranking: 367, league: league_6, user: user_13, photo: cornwall_logo_url },
   { name: 'Atlantic Breakers B', ranking: 342, league: league_6, user: user_14, photo: cornwall_b_logo_url },
@@ -89,7 +107,16 @@ teams = Team.create!([
   { name: 'C-Stars', ranking: 249, league: league_7, user: user_17, photo: stockholm_logo_url },
   { name: 'Sisters of Mersey', ranking: 74, league: league_8, user: user_18, photo: liverpool_logo_url },
   { name: 'Yellow Shovemarines', ranking: 195, league: league_8, user: user_19, photo: yellow_shovemarines_logo_url },
-  { name: 'C Team', ranking: 360, league: league_8, user: user_20, photo: liverpool_logo_url }
+  { name: 'C Team', ranking: 360, league: league_8, user: user_20, photo: liverpool_logo_url },
+  { name: 'All Stars', ranking: 24, league: league_9, user: user_21, photo: amsterdam_logo_url },
+  { name: 'Battle Stars', ranking: 338, league: league_9, user: user_22, photo: amsterdam_logo_url },
+  { name: 'Toutes les Etoiles', ranking: 133, league: league_10, user: user_23, photo: brest_logo_url },
+  { name: 'Les Sabordeuses', ranking: 210, league: league_10, user: user_24, photo: brest_logo_url},
+  { name: 'All Stars', ranking: 40, league: league_11, user: user_25, photo: brussels_logo_url},
+  { name: 'Atomium Kittens', ranking: 159, league: league_11, user: user_26, photo: brussels_b_logo_url},
+  { name: 'All Stars', ranking: 40, league: league_11, user: user_25, photo: brussels_logo_url},
+  { name: 'Atomium Kittens', ranking: 159, league: league_11, user: user_26, photo: brussels_b_logo_url},
+
 ])
 
 puts "#{Team.count} teams created"
