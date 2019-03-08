1c112692b3fc6a02dd6b0ca96bbc2a7a1056a029
mathilde ferrey <mathildeferrey@yahoo.com>
2019-03-08 11:07:43 +0100

    monday on search datepicker

--- app/javascript/plugins/flatpickr.js
+++ app/javascript/plugins/flatpickr.js
@@ -1,8 +1,7 @@
 import flatpickr from "flatpickr"
 import "flatpickr/dist/flatpickr.min.css" // Note this is important!
-
-flatpickr(".datepicker", {inline:true})
 flatpickr.l10ns.default.firstDayOfWeek = 1; // Monday
+flatpickr(".datepicker", {inline:true})
 
 const disabled = document.querySelector(".datepicker-unavailable")
 
